63823382272cf440647a897af9a202fec45c897d
Utej <91786744+utej8553@users.noreply.github.com>
2026-08-28 16:36:16 +0530

    Update elasticache.sh

--- aws-config/elasticache.sh
+++ aws-config/elasticache.sh
@@ -1,1 +1,5 @@
-
+#!/bin/bash
+REGION=$(aws configure get region)
+CACHE_NAME="student-cache"
+aws elasticache create-serverless-cache --serverless-cache-name "$CACHE_NAME" --engine valkey --region "$REGION"
+echo "Cache: $CACHE_NAME"
