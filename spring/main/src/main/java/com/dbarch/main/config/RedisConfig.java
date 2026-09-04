563654f7d85d0be4d9083d534cc5fb5dabe85ad2
Utej <91786744+utej8553@users.noreply.github.com>
2026-09-04 23:37:51 +0530

    Update RedisConfig.java

--- spring/main/src/main/java/com/dbarch/main/config/RedisConfig.java
+++ spring/main/src/main/java/com/dbarch/main/config/RedisConfig.java
@@ -3,22 +3,36 @@ package com.dbarch.main.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
+
     @Value("${aws.elasticache.endpoint}")
     private String endpoint;
+
     @Value("${aws.elasticache.port}")
     private int port;
+
     @Bean
+    @Primary
     public LettuceConnectionFactory elasticacheConnectionFactory() {
-        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(endpoint, port);
-        return new LettuceConnectionFactory(config);
+        RedisStandaloneConfiguration config =
+                new RedisStandaloneConfiguration(endpoint, port);
+
+        LettuceClientConfiguration clientConfig =
+                LettuceClientConfiguration.builder()
+                        .useSsl()
+                        .build();
+
+        return new LettuceConnectionFactory(config, clientConfig);
     }
+
     @Bean
     public RedisTemplate<String, String> elasticacheTemplate() {
         RedisTemplate<String, String> template = new RedisTemplate<>();
