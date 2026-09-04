4e81bbbd640482b9dcc83d6bf355adfa50208ed5
Utej <91786744+utej8553@users.noreply.github.com>
2026-09-04 16:42:03 +0530

    added studentcontroller

---
+++ spring/main/src/main/java/com/dbarch/main/controller/StudentController.java
@@ -0,0 +1,50 @@
+package com.dbarch.main.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dbarch.main.model.Student;
+import com.dbarch.main.service.StudentService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+
+@RestController
+@RequestMapping("/students")
+public class StudentController {
+    private final StudentService studentService;
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+    @PostMapping
+    public ResponseEntity<String> postMethodName(@RequestBody Student student) {
+        studentService.save(student);
+        return ResponseEntity.ok("Student saved successfully");
+    }
+    @GetMapping("/{roll}")
+    public ResponseEntity<Student> getMethodName(@RequestParam String roll) {
+        Student student = studentService.findByRoll(roll);
+        if(student == null){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(student);
+    }
+    @GetMapping
+    public ResponseEntity<List<Student>> findAll() {
+        return ResponseEntity.ok(studentService.findAll());
+    }
+    @DeleteMapping("/{roll}")
+    public ResponseEntity<String> delete(@PathVariable String roll) {
+        studentService.delete(roll);
+        return ResponseEntity.ok("Student deleted successfully");
+    }
+}
