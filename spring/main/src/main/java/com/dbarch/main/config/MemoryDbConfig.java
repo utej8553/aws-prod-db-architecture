4082b856b660ddb5bb34a1bec900cf020d6b9225
Utej <91786744+utej8553@users.noreply.github.com>
2026-09-04 23:37:33 +0530

    Update MemoryDbConfig.java

--- spring/main/src/main/java/com/dbarch/main/config/MemoryDbConfig.java
+++ spring/main/src/main/java/com/dbarch/main/config/MemoryDbConfig.java
@@ -3,22 +3,44 @@ package com.dbarch.main.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.RedisClusterConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class MemoryDbConfig {
+
     @Value("${aws.memorydb.endpoint}")
     private String endpoint;
+
     @Value("${aws.memorydb.port}")
     private int port;
+
+    @Value("${aws.memorydb.username}")
+    private String username;
+
+    @Value("${aws.memorydb.password}")
+    private String password;
+
     @Bean
     public LettuceConnectionFactory memoryDbConnectionFactory() {
-        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(endpoint, port);
-        return new LettuceConnectionFactory(config);
+        RedisClusterConfiguration config =
+                new RedisClusterConfiguration();
+
+        config.clusterNode(endpoint, port);
+        config.setUsername(username);
+        config.setPassword(password);
+
+        LettuceClientConfiguration clientConfig =
+                LettuceClientConfiguration.builder()
+                        .useSsl()
+                        .build();
+
+        return new LettuceConnectionFactory(config, clientConfig);
     }
+
     @Bean
     public RedisTemplate<String, String> memoryDbTemplate() {
         RedisTemplate<String, String> template = new RedisTemplate<>();
